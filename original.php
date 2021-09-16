fe5d79dd256c9536c0eff2b786e67178037ffa79
Virgiliu Plesca <virgiliu.plesca@iis.utm.md>
2021-09-16 20:44:27 +0300

    Formated the text from tale.php

--- original.php
+++ original.php
@@ -1,31 +1,54 @@
 <!Doctype html>
 <html lang="ro">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
                 user-scalable=no,
                 initial-scale=1.0,
                 maximum-scale=1.0,
                 minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="assets/style.css">
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
-        <title>Original</title>
-    </head>
+    <title>Original</title>
+</head>
 
-    <body>
+<body>
 
-        <?php
-            include('nav.php');
-        ?>
+    <?php
+    include('nav.php');
+    ?>
 
-        <section id="main_section">
-            <div class="container">
+    <section id="main_sect_original">
+        <div class="container">
+            <div id="content">
+                <div id="header">
+                    <h1>
+                        Prostia omeneacsa
+                    </h1>
+                    <span>
+                        de Ion Creangă
+                    </span>
+                </div>
 
+                <br>
+                <br>
+                <br>
+                <img id="main_img" src="./img/homeImg.jpg" alt="Prostia omeneasca" align="right">
+
+                <div id="tale">
+                    <span>
+                        <?php
+                        include("tale.php");
+                        ?>
+                    </span>
+                </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-    </body>
+</body>
+
 </html>
